8d7f2814943b3cb931036617400cec23357383d8
itsshruti14 <shrutireddy418@gmail.com>
2026-02-27 14:53:24 +0530

    Add task model class

--- To-do-list/src/main/java/com/example/To_do/list/task.java
+++ To-do-list/src/main/java/com/example/To_do/list/task.java
@@ -1,25 +1,9 @@
 package com.example.To_do.list;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+public class task {
 
-@Entity
-public class Task {
-    @Id
-    @GeneratedValue
-    private Long id;
-    private String description;
+    private int id;
+    private String title;
+    private boolean completed;
 
-    public Task() {}
-
-    public Task(String description) {
-        this.description = description;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
 }
